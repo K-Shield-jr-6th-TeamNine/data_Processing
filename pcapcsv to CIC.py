b37e25524f137fe3bcc93f1a594b6a8fdbcd6323
Tarrow9 <gkgkrnjs123@naver.com>
2021-05-21 14:57:28 +0900

    Update pcapcsv to CIC.py
    Feature format fix

--- pcapcsv to CIC.py
+++ pcapcsv to CIC.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-file_Path = "C:/Users/Tarrow/Desktop/VScode/codes/lresult.csv"
+file_Path = "~Desktop/lresult.csv"
 # file path that CSV file from changing the pcap file with cicflowmeter
+# need to customizing
 
-pcap_data = pd.read_csv(file_Path, names=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82])
+pcap_data = pd.read_csv(file_Path, names=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83])
 # column naming
 
 testing_data = pcap_data[[4,5,6,7,12,13,14,15,16,17,18,19,20,21,22,23,8,9,33,36,34,35,37,40,41,38,39,42,45,46,43,44,47,48,49,50,29,30,10,11,25,24,26,27,28,51,52,53,54,55,56,78,57,58,59,76,77,70,71,74,72,73,75,79,81,80,82,60,61,32,31,64,65,62,63,68,69,66,67]]
@@ -11,3 +12,8 @@ testing_data = pcap_data[[4,5,6,7,12,13,14,15,16,17,18,19,20,21,22,23,8,9,33,36,
 
 testing_data.to_csv("./test_data.csv", header = False, index = False)
 # save file
+
+f = open("./test_data.csv","r+") # overwriting file, SO IF YOU TRY THIS ONCE, next one must diffrent file
+f.write("Dst Port,Protocol,Timestamp,Flow Duration,Tot Fwd Pkts,Tot Bwd Pkts,TotLen Fwd Pkts,TotLen Bwd Pkts,Fwd Pkt Len Max,Fwd Pkt Len Min,Fwd Pkt Len Mean,Fwd Pkt Len Std,Bwd Pkt Len Max,Bwd Pkt Len Min,Bwd Pkt Len Mean,Bwd Pkt Len Std,Flow Byts/s,Flow Pkts/s,Flow IAT Mean,Flow IAT Std,Flow IAT Max,Flow IAT Min,Fwd IAT Tot,Fwd IAT Mean,Fwd IAT Std,Fwd IAT Max,Fwd IAT Min,Bwd IAT Tot,Bwd IAT Mean,Bwd IAT Std,Bwd IAT Max,Bwd IAT Min,Fwd PSH Flags,Bwd PSH Flags,Fwd URG Flags,Bwd URG Flags,Fwd Header Len,Bwd Header Len,Fwd Pkts/s,Bwd Pkts/s,Pkt Len Min,Pkt Len Max,Pkt Len Mean,Pkt Len Std,Pkt Len Var,FIN Flag Cnt,SYN Flag Cnt,RST Flag Cnt,PSH Flag Cnt,ACK Flag Cnt,URG Flag Cnt,CWE Flag Count,ECE Flag Cnt,Down/Up Ratio,Pkt Size Avg,Fwd Seg Size Avg,Bwd Seg Size Avg,Fwd Byts/b Avg,Fwd Pkts/b Avg,Fwd Blk Rate Avg,Bwd Byts/b Avg,Bwd Pkts/b Avg,Bwd Blk Rate Avg,Subflow Fwd Pkts,Subflow Fwd Byts,Subflow Bwd Pkts,Subflow Bwd Byts,Init Fwd Win Byts,Init Bwd Win Byts,Fwd Act Data Pkts,Fwd Seg Size Min,Active Mean,Active Std,Active Max,Active Min,Idle Mean,Idle Std,Idle Max,Idle Min")
+f.close()
+# feature renaming to Kaggle format (except "Label" feature)
